e6d3c28a030a97b878812e3a8b11bb1e30b5f606
Ivan Garcia <ivangm180@gmail.com>
2021-07-05 05:46:17 -0500

    modifying the animation

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import logo from "./assets/Scarecrow.png"
 function App() {
   return (
     <div className="App">
-      <Title className="title">I have bad news for you</Title>
-      <Text className="text" >The page you are looking for might be removed or is temporarily unavailable.</Text>
-      <Button className="button" type="button">BACK TO HOMEPAGE</Button>
       <div className="image">
-      <Image src={logo} />
+        <Image src={logo} />
+      </div>
+      <div className="content">
+        <Title className="title">I have bad news for you</Title>
+        <Text className="text" >The page you are looking for might be removed or is temporarily unavailable.</Text>
+        <Button className="button" type="button">BACK TO HOMEPAGE</Button>
       </div>
 
     </div>
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const Image = styled.img `{
-  width: 600px;
-  animation: bubble 7s linear infinite;
+  width: 100%;
+  min-width: 400px;
+  animation: bubble 3s linear infinite;
 
 }`;
